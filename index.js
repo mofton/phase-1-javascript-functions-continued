eecfcdb69bd3629dc600f6147a5cf333cbb3f658
Mofeid Moshreky <mof_ton@yahoo.com>
2023-04-04 22:14:16 -0400

    compleate lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = "*") {
+  return function (adj = "special") {
+    return `You are ${flair}${adj}${flair}!`;
+  };
+}
